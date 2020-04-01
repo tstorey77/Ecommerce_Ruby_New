0201f0c81c63d127d1ee033e5075e1c575022b13
tstorey77 <tsstorey13@gmail.com>
2020-04-01 15:00:56 -0500

    Got project to normal state and added a_a:resource

---
+++ app/admin/cards.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Card do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :name, :card_type, :description, :attack, :defence, :price
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:name, :card_type, :description, :attack, :defence, :price]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  resources :cards
+
+  get 'cards', to: 'cards#show', as: 'show'
+
+  root to: 'cards#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
