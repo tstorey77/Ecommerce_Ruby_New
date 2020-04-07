162f8108b38c3376d5cb40bca36637c275da5239
tstorey77 <tsstorey13@gmail.com>
2020-04-07 18:37:12 -0500

    added ability to login and save session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,12 +6,12 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(username: params[:username])
     if @user&.authenticate(params[:password])
-      sessions[:user_id] = @user.id
+      session[:user_id] = @user.id
       redirect_to '/welcome'
     else
       redirect_to '/login'
     end
- end
+  end
 
   def page_requires_login; end
 
