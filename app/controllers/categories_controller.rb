92fc44a886f2f756a7b9f58de9be6dcc008a6152
tstorey77 <tsstorey13@gmail.com>
2020-04-06 20:17:36 -0500

    added links to categories from _card partial

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,17 @@
+# frozen_string_literal: true
+
 class CategoriesController < InheritedResources::Base
+  def index
+    @category = Category.all
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
 
   private
 
-    def category_params
-      params.require(:category).permit(:name)
-    end
-
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
