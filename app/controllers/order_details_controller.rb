366596e847b53426dbae1cb5a293f1888c46a598
tstorey77 <tsstorey13@gmail.com>
2020-04-14 11:57:17 -0500

    Initital Commit

--- app/controllers/order_details_controller.rb
+++ app/controllers/order_details_controller.rb
@@ -1,9 +1,27 @@
+# frozen_string_literal: true
+
 class OrderDetailsController < InheritedResources::Base
+  before_action :initialize_session
+  before_action :load_cart
+
+  def remove_from_cart
+    id = params[:id].to_i
+    session[:cart].delete(id)
+
+    redirect_to order_details_path
+  end
 
   private
 
-    def order_detail_params
-      params.require(:order_detail).permit(:Cards_id, :Order_id, :quantity, :price)
-    end
+  def order_detail_params
+    params.require(:order_detail).permit(:Cards_id, :Order_id, :quantity, :price)
+  end
 
+  def initialize_session
+    session[:cart] ||= []
+  end
+
+  def load_cart
+    @cart = Card.find(session[:cart])
+  end
 end
